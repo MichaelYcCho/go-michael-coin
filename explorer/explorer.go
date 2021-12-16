6289e859a00b75909b6c99d72602eaf177029291
Youngchan <michael.yc.cho@gmail.com>
2021-12-16 23:15:15 +0900

    feature/Refactoring

---
+++ explorer/explorer.go
@@ -0,0 +1,48 @@
+package explorer
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+
+	"github.com/michael_cho77/go-michael-coin/blockchain"
+)
+
+const (
+	port        string = ":8000"
+	templateDir string = "explorer/templates/"
+)
+
+var templates *template.Template
+
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	templates.ExecuteTemplate(rw, "home", data)
+}
+
+func add(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(rw, "add", nil)
+	case "POST":
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockchain().AddBlock(data)
+		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
+	}
+}
+
+func Start() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
+	fmt.Printf("Listening on http://ttp://127.0.0.1%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
--- main.go
+++ main.go
@@ -1,29 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
-	"log"
-	"net/http"
-
-	"github.com/michael_cho77/go-michael-coin/blockchain"
+	"github.com/michael_cho77/go-michael-coin/explorer"
 )
 
-const port string = ":8000"
-
-type homeData struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
-}
-
-func home(rw http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/home.gohtml"))
-	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
-	tmpl.Execute(rw, data)
-}
-
 func main() {
-	http.HandleFunc("/", home)
-	fmt.Printf("Listening on http://127.0.0.1%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	explorer.Start()
 }
