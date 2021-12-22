ee7577e47daf89a3e6a49c23200658163af5a0dc
Youngchan <michael.yc.cho@gmail.com>
2021-12-22 23:38:46 +0900

    feature/Json Decode

--- main.go
+++ main.go
@@ -5,11 +5,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/michael_cho77/go-michael-coin/blockchain"
+	"github.com/michael_cho77/go-michael-coin/utils"
 )
 
 const port string = ":8000"
 
 type URL string
+
+type AddBlockBody struct {
+	Message string
+}
 
 func (u URL) MarshalText() ([]byte, error) {
 	url := fmt.Sprintf("http://localhost%s%s", port, u)
@@ -26,23 +33,47 @@ type URLDescription struct {
 func documentation(rw http.ResponseWriter, r *http.Request) {
 	data := []URLDescription{
 		{
-			URL:         "/",
+			URL:         URL("/"),
 			Method:      "GET",
 			Description: "See Documentation",
 		},
 		{
-			URL:         "/blocks",
+			URL:         URL("/blocks"),
+			Method:      "GET",
+			Description: "See All Blocks",
+		},
+		{
+			URL:         URL("/blocks"),
 			Method:      "POST",
 			Description: "Add A Block",
 			Payload:     "data:string",
+		},
+		{
+			URL:         URL("/blocks/{id}"),
+			Method:      "GET",
+			Description: "See A Block",
 		},
 	}
 	rw.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(rw).Encode(data)
 }
 
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
+	case "POST":
+		var addBlockBody AddBlockBody
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
+		rw.WriteHeader(http.StatusCreated)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", documentation)
+	http.HandleFunc("/blocks", blocks)
 	fmt.Printf("Listening on http://127.0.0.1%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
