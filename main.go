7e9b1d57a1b96bfdb0361501ea5e7ab2959e5db8
Youngchan <michael.yc.cho@gmail.com>
2022-04-12 21:44:33 +0900

    feature/Parsing Commands

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
-	"github.com/michael_cho77/go-michael-coin/explorer"
-	"github.com/michael_cho77/go-michael-coin/rest"
+	"fmt"
+	"os"
 )
 
+func usage() {
+	fmt.Printf("Welcome to Michael-Coin\n\n")
+	fmt.Printf("Please use the following commands:\n\n")
+	fmt.Printf("explorer:	Start the HTML Explorer\n")
+	fmt.Printf("rest:		Start the REST API (recommended)\n\n")
+	os.Exit(0)
+}
+
 func main() {
-	go explorer.Start(3000)
-	rest.Start(8000)
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Println("Start Explorer")
+	case "rest":
+		fmt.Println("Start REST API")
+	default:
+		usage()
+	}
 }
