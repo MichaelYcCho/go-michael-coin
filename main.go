59502b18b7b76676ce4e836a37a4e0885e3f54d1
Youngchan <michael.yc.cho@gmail.com>
2022-04-12 22:12:06 +0900

    feature/FalgSet

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
@@ -18,12 +19,21 @@ func main() {
 		usage()
 	}
 
+	rest := flag.NewFlagSet("rest", flag.ExitOnError)
+
+	portFlag := rest.Int("port", 4000, "Sets the port of the server")
+
 	switch os.Args[1] {
 	case "explorer":
 		fmt.Println("Start Explorer")
 	case "rest":
-		fmt.Println("Start REST API")
+		rest.Parse(os.Args[2:])
 	default:
 		usage()
+	}
+
+	if rest.Parsed() {
+		fmt.Printf("Start Server")
+		fmt.Println(portFlag)
 	}
 }
