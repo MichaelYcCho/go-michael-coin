93c7324bb5ca2f609f1832dae953786b2bb90d31
Youngchan <michael.yc.cho@gmail.com>
2022-04-12 22:40:14 +0900

    feature/Flag

--- main.go
+++ main.go
@@ -1,39 +1,9 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
+	"github.com/michael_cho77/go-michael-coin/cli"
 )
 
-func usage() {
-	fmt.Printf("Welcome to Michael-Coin\n\n")
-	fmt.Printf("Please use the following commands:\n\n")
-	fmt.Printf("explorer:	Start the HTML Explorer\n")
-	fmt.Printf("rest:		Start the REST API (recommended)\n\n")
-	os.Exit(0)
-}
-
 func main() {
-	if len(os.Args) < 2 {
-		usage()
-	}
-
-	rest := flag.NewFlagSet("rest", flag.ExitOnError)
-
-	portFlag := rest.Int("port", 4000, "Sets the port of the server")
-
-	switch os.Args[1] {
-	case "explorer":
-		fmt.Println("Start Explorer")
-	case "rest":
-		rest.Parse(os.Args[2:])
-	default:
-		usage()
-	}
-
-	if rest.Parsed() {
-		fmt.Printf("Start Server")
-		fmt.Println(portFlag)
-	}
+	cli.Start()
 }
