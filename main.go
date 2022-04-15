e649b96ee3c7f78f39b9136df7926ddf3d17760c
Youngchan <michael.yc.cho@gmail.com>
2022-04-16 01:51:59 +0900

    feature/Restoring the BlockChain

--- main.go
+++ main.go
@@ -3,7 +3,5 @@ package main
 import "github.com/michael_cho77/go-michael-coin/blockchain"
 
 func main() {
-	blockchain.Blockchain().AddBlock("First")
-	blockchain.Blockchain().AddBlock("Second")
-	blockchain.Blockchain().AddBlock("Third")
+	blockchain.Blockchain()
 }
