2890e79c7f640701b9df1ef2ab75d90d6799edce
Youngchan <michael.yc.cho@gmail.com>
2022-04-15 23:47:16 +0900

    feature/Persisting The BlockChain

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,9 +1,7 @@
 package blockchain
 
 import (
-	"bytes"
 	"crypto/sha256"
-	"encoding/gob"
 	"fmt"
 
 	"github.com/michael_cho77/go-michael-coin/db"
@@ -17,18 +15,8 @@ type Block struct {
 	Height   int    `json:"height"`
 }
 
-func (b *Block) toByte() []byte {
-	var blockBuffer bytes.Buffer
-	encoder := gob.NewEncoder(&blockBuffer)
-	encoder.Encode(b)
-	utils.HandleErr(encoder.Encode(b))
-	return blockBuffer.Bytes()
-
-}
-
 func (b *Block) persist() {
-	db.SaveBlock(b.Hash, b.toByte())
-
+	db.SaveBlock(b.Hash, utils.ToBytes(b))
 }
 
 func createBlock(data string, prevHash string, height int) *Block {
--- main.go
+++ main.go
@@ -3,5 +3,7 @@ package main
 import "github.com/michael_cho77/go-michael-coin/blockchain"
 
 func main() {
-	blockchain.Blockchain()
+	blockchain.Blockchain().AddBlock("First")
+	blockchain.Blockchain().AddBlock("Second")
+	blockchain.Blockchain().AddBlock("Third")
 }
