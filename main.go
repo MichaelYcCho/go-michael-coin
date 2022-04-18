2ba8e298a33ba9611c7c138572c0cfdec08fc228
Youngchan <michael.yc.cho@gmail.com>
2022-04-18 23:21:19 +0900

    feature/Mine Block

--- main.go
+++ main.go
@@ -1,22 +1,11 @@
 package main
 
 import (
-	"crypto/sha256"
-	"fmt"
-	"strings"
+	"github.com/michael_cho77/go-michael-coin/cli"
+	"github.com/michael_cho77/go-michael-coin/db"
 )
 
 func main() {
-	difficulty := 2
-	target := strings.Repeat("0", difficulty)
-	nonce := 1
-	for {
-		hash := fmt.Sprintf("%x", sha256.Sum256(([]byte("hello" + fmt.Sprint(nonce)))))
-		fmt.Printf("Hash:%s\nTarget:%s\nNonce:%d\n\n", hash, target, nonce)
-		if strings.HasPrefix(hash, target) {
-			return
-		} else {
-			nonce++
-		}
-	}
+	defer db.Close()
+	cli.Start()
 }
