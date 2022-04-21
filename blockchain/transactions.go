c2d4fe17985b8ba47aa9d5fa417dcb637e1fe8a4
Youngchan <michael.yc.cho@gmail.com>
2022-04-22 00:01:44 +0900

    featuer/AddTx

--- blockchain/transactions.go
+++ blockchain/transactions.go
@@ -1,6 +1,7 @@
 package blockchain
 
 import (
+	"errors"
 	"time"
 
 	"github.com/michael_cho77/go-michael-coin/utils"
@@ -9,6 +10,12 @@ import (
 const (
 	minerReward int = 50
 )
+
+type mempool struct {
+	Txs []*Tx
+}
+
+var Mempool *mempool = &mempool{}
 
 type Tx struct {
 	Id        string   `json:"id"`
@@ -46,4 +53,22 @@ func makeCoinbaseTx(address string) *Tx {
 	}
 	tx.getId()
 	return &tx
+}
+
+// 실제로 트랜젝션을 생성해줄 리비서
+func makeTx(from, to string, amount int) (*Tx, error) {
+	if Blockchain().BalanceByAddress(from) < amount {
+		return nil, errors.New("not enough money")
+	}
+	return _, _
+}
+
+// Mempool을 위해서 사용될 리시버
+func (m *mempool) AddTx(to string, amount int) error {
+	tx, err := makeTx("nico", to, amount)
+	if err != nil {
+		return err
+	}
+	m.Txs = append(m.Txs, tx)
+	return nil
 }
