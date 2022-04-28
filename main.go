d00c65aa646da5a4dedd4146abcacf6bf7b76d54
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-04-28 23:46:44 +0900

    feature/Persist Wallet

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/michael_cho77/go-michael-coin/wallet"
 
 func main() {
-	wallet.Start()
+	wallet.Wallet()
 }
--- wallet/wallet.go
+++ wallet/wallet.go
@@ -2,7 +2,16 @@ package wallet
 
 import (
 	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
 	"os"
+
+	"github.com/michael_cho77/go-michael-coin/utils"
+)
+
+const (
+	fileName string = "michaelcoin.wallet"
 )
 
 type wallet struct {
@@ -16,12 +25,32 @@ func hasWalletFile() bool {
 	return !os.IsNotExist(err)
 }
 
+func createPrivateKey() *ecdsa.PrivateKey {
+	privKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+
+	utils.HandleErr(err)
+
+	return privKey
+}
+
+func persistKey(key *ecdsa.PrivateKey) {
+	bytes, err := x509.MarshalECPrivateKey(key)
+	utils.HandleErr(err)
+	err = os.WriteFile(fileName, bytes, 0644)
+	utils.HandleErr(err)
+}
+
 func Wallet() *wallet {
 	if w == nil {
+		w = &wallet{}
 		if hasWalletFile() {
 			// yes -> restore from file
+		} else {
+			// no -> create prv key, save to file
+			key := createPrivateKey()
+			persistKey(key)
+			w.privateKey = key
 		}
-		// no -> create prv key, save to file
 	}
 	return w
 }
