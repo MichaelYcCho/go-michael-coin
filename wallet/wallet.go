aa5a4a393fd14d7c553394b2411cda400e0c9842
Youngchan <michael.yc.cho@gmail.com>
2022-05-01 16:48:56 +0900

    feature/ Verification Function

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -7,13 +7,14 @@ import (
 	"crypto/x509"
 	"encoding/hex"
 	"fmt"
+	"math/big"
 	"os"
 
 	"github.com/michael_cho77/go-michael-coin/utils"
 )
 
 const (
-	fileName string = "michaelcoin.wallet"
+	fileName string = "nomadcoin.wallet"
 )
 
 type wallet struct {
@@ -24,15 +25,13 @@ type wallet struct {
 var w *wallet
 
 func hasWalletFile() bool {
-	_, err := os.Stat("michaelcoin.wallet")
+	_, err := os.Stat(fileName)
 	return !os.IsNotExist(err)
 }
 
-func createPrivateKey() *ecdsa.PrivateKey {
+func createPrivKey() *ecdsa.PrivateKey {
 	privKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-
 	utils.HandleErr(err)
-
 	return privKey
 }
 
@@ -51,9 +50,13 @@ func restoreKey() (key *ecdsa.PrivateKey) {
 	return
 }
 
-func aFromK(key *ecdsa.PrivateKey) string {
-	z := append(key.X.Bytes(), key.Y.Bytes()...)
+func encodeBigInts(a, b []byte) string {
+	z := append(a, b...)
 	return fmt.Sprintf("%x", z)
+}
+
+func aFromK(key *ecdsa.PrivateKey) string {
+	return encodeBigInts(key.X.Bytes(), key.Y.Bytes())
 }
 
 func sign(payload string, w *wallet) string {
@@ -61,19 +64,45 @@ func sign(payload string, w *wallet) string {
 	utils.HandleErr(err)
 	r, s, err := ecdsa.Sign(rand.Reader, w.privateKey, payloadAsB)
 	utils.HandleErr(err)
-	signature := append(r.Bytes(), s.Bytes()...)
-	return fmt.Sprintf("%x", signature)
+	return encodeBigInts(r.Bytes(), s.Bytes())
+}
+
+func restoreBigInts(payload string) (*big.Int, *big.Int, error) {
+	bytes, err := hex.DecodeString(payload)
+	if err != nil {
+		return nil, nil, err
+	}
+	firstHalfBytes := bytes[:len(bytes)/2]
+	sencodHalfBytes := bytes[len(bytes)/2:]
+	bigA, bigB := big.Int{}, big.Int{}
+	bigA.SetBytes(firstHalfBytes)
+	bigB.SetBytes(sencodHalfBytes)
+	return &bigA, &bigB, nil
+}
+
+func verify(signature, payload, address string) bool {
+	r, s, err := restoreBigInts(signature)
+	utils.HandleErr(err)
+	x, y, err := restoreBigInts(address)
+	utils.HandleErr(err)
+	publicKey := ecdsa.PublicKey{
+		Curve: elliptic.P256(),
+		X:     x,
+		Y:     y,
+	}
+	payloadBytes, err := hex.DecodeString(payload)
+	utils.HandleErr(err)
+	ok := ecdsa.Verify(&publicKey, payloadBytes, r, s)
+	return ok
 }
 
 func Wallet() *wallet {
 	if w == nil {
 		w = &wallet{}
 		if hasWalletFile() {
-			// yes -> restore from file
 			w.privateKey = restoreKey()
 		} else {
-			// no -> create prv key, save to file
-			key := createPrivateKey()
+			key := createPrivKey()
 			persistKey(key)
 			w.privateKey = key
 		}
