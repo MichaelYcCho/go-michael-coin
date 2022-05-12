7bcf559bb73fa24920b08d4297f196ac43d7d077
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-05-12 22:26:29 +0900

    feature/Newest Block

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -29,6 +29,7 @@ func AddPeer(address, port, openPort string) {
 	conn, _, err := websocket.DefaultDialer.Dial(
 		fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
-	initPeer(conn, address, port)
+	p := initPeer(conn, address, port)
+	sendNewestBlock(p)
 
 }
